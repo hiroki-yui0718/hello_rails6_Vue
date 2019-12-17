e342717b20155019ef893ada8e88bfaa17398b9a
Hiroki Yui <xxx_yyy_www_1234@yahoo.co.jp>
2019-12-17 19:39:22 +0900

    Grid5

--- app/mailboxes/comments_mailbox.rb
+++ app/mailboxes/comments_mailbox.rb
@@ -1,20 +1,2 @@
 class CommentsMailbox < ApplicationMailbox
-  before_processing :validate_request
-  def process
-    board.comments.create!(body: mail.decode, creator: commenter)
-  end
-  def validate_request
-    return if commenter && board
-    bounce_with CommentMailer.invalid_request(
-    inbound_email, commenter: commenter, board: board
-    )
-  end
-  def commenter
-    return @commenter if defined?(@commenter)
-    @commenter = User.find_by(email: mail.from)
-  end
-  def board
-    return @board if defined?(@board)
-    @board = Board.find_by(id: mail.to[0].split('-')[0]
-  end
 end
